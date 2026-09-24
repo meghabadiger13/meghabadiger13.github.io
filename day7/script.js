ee8c3ff8c758d01c5ef848e06aab007931869869
meghabadiger13 <badiger@email.sc.edu>
2026-09-24 13:27:34 -0400

    lab 7

--- day7/script.js
+++ day7/script.js
@@ -4,28 +4,24 @@ const damageAmount = 30;
 function takeDamage() {
     playerHealth = playerHealth - damageAmount;
 
-    const healthText = document.getElementById("health-disparity");
+    const healthText = document.getElementById("health-display");
     const statusText = document.getElementById("status-message");
 
-    healthText.innerText = playerHealth;
-
-    if (playerHealth > 0) 
-        {
-            healthText.innerText = playerHealth;
-            statusText.innerText = "You've been hit!";
-        
+    if(playerHealth > 0)
+    {
+        healthText.innerText = playerHealth;
+        statusText.innerText = "You've been hit!";
     }
     else
     {
         healthText.innerText = 0;
         statusText.innerText = "Game Over!";
         statusText.style.color = "#f9331d";
-        statusText.style.fontweight = "bold";
+        statusText.style.fontWeight = "bold";
 
-        document.body.style.backgroundColor = "#Salala";
+        document.body.style.backgroundColor = "#5a1a1a";
 
         document.querySelector("button").disabled = true;
-        document.querySelector("button").InnerText = "Dead";
-
+        document.querySelector("button").innerText = "Dead";
     }
 }
---
+++ lab5/script.js
@@ -0,0 +1,31 @@
+let playerHealth = 100;
+const damageAmount = 30;
+
+function takeDamage() {
+    playerHealth = playerHealth - damageAmount;
+
+    const healthText = document.getElementById("health-disparity");
+    const statusText = document.getElementById("status-message");
+
+    healthText.innerText = playerHealth;
+
+    if (playerHealth > 0) 
+        {
+            healthText.innerText = playerHealth;
+            statusText.innerText = "You've been hit!";
+        
+    }
+    else
+    {
+        healthText.innerText = 0;
+        statusText.innerText = "Game Over!";
+        statusText.style.color = "#f9331d";
+        statusText.style.fontweight = "bold";
+
+        document.body.style.backgroundColor = "#Salala";
+
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").InnerText = "Dead";
+
+    }
+}
